d670e43ed58150cd48975b06ddd2c8c61bce9fdf
SAMahlangu <114006153+SAMahlangu@users.noreply.github.com>
2024-08-21 10:36:57 +0200

    Update VideoDisplay.js

--- src/components/VideoDisplay.js
+++ src/components/VideoDisplay.js
@@ -9,7 +9,7 @@ const VideoDisplay = ({ uploadSuccessful, resetUpload }) => {
   useEffect(() => {
     if (uploadSuccessful && !source) {
       if (!!window.EventSource) {
-        const newSource = new EventSource('http://13.60.17.80/stream');
+        const newSource = new EventSource('http://13.51.20.104/stream');
         
         newSource.onmessage = (event) => {
           // Parse the JSON data to extract frame and count
